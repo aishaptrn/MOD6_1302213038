60a3ce9d0570b452ade87f445195cb172341ba2c
HelloWorldYessir420 <91200696+aishaptrn@users.noreply.github.com>
2023-03-27 08:31:49 +0700

    “penambahan design by contract pada semua kelas

--- mod6_1302213038/SayaTubeUser.cs
+++ mod6_1302213038/SayaTubeUser.cs
@@ -5,6 +5,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
 
 namespace mod6_1302213038
 {
@@ -16,6 +17,9 @@ namespace mod6_1302213038
 
         public SayaTubeUser(string username)
         {
+            Contract.Requires(username.Length <= 100);
+            Contract.Requires(username != null);
+            
             this.username = username;
             this.uploadedVideos = new List<SayaTubeVideo>();
         }
@@ -25,20 +29,24 @@ namespace mod6_1302213038
             int sum = 0;
             for (int i = 0; i < uploadedVideos.Count; i++)
             {
-                sum += uploadedVideos[i].getPlayCount();
+                sum += uploadedVideos[i].getPlayCount();                
             }
             return sum;
         }
 
         public void addVideo(SayaTubeVideo video)
         {
+            Contract.Requires(video != null);
+            Contract.Requires(video.getPlayCount() < int.MaxValue);
+
             this.uploadedVideos.Add(video);
         }
 
         public void printAllVideoPlayCount()
         {
+            Contract.Requires(uploadedVideos.Count() < 9);
+            
             Console.WriteLine("User\t\t: Aish");
-
             for (int i = 0; i < uploadedVideos.Count; i++)
             {
                 Console.WriteLine("Video " + (i+1) + " judul\t: " + uploadedVideos[i].getTitle());
